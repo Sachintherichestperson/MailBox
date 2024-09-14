25257b9a32dbc7878da80da21dd7de8024dff205
sachin bajaj <bajajsachin100@gmail.com>
2024-09-14 22:21:44 +0530

    saved

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
+const config = require("config")
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/ecommerce")
+mongoose
+.connect(`${config.get("MONGODB_URI")}/ecommerce`)
 .then(function(){
-    console.log("Connected")
+    dbgr("Connected")
 })
 .catch(function(err){
-    console.log(err)
+    dbgr(err)
 })
 
-module.exports = mongoose.Connection;
+module.exports = mongoose.connection;
