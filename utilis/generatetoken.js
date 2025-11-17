2a8658737b2384e8f79dfc90ef057963763f63bf
Sachin Bajaj <bajajsachin101@gmail.com>
2025-11-17 14:30:32 +0530

    Less Alert

--- utilis/generatetoken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generatetoken = (user) => {
-    return jwt.sign({email: user.email, id: user._id}, process.env.JWT_KEY);
-}
-
-
-module.exports.generatetoken = generatetoken;
